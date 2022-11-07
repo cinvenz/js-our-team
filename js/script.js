d7c1baaf59d5bdef21cbf7732aeff142cc5eb311
cinvenz <vincenzobortune@gmail.com>
2022-11-07 17:15:24 +0100

    inseriti i 3 oggetti nel dom

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const cardsTeam = document.querySelector('.cards-team')
+
+const arrTeam = [
+	{
+		name: 'Wayne Barnett',
+		work: 'Founder & CEO',
+		photo: 'wayne-barnett-founder-ceo.jpg',
+	},
+	{
+		name: 'Angela Caroll',
+		work: 'Chief Editor',
+		photo: 'angela-caroll-chief-editor.jpg',
+	},
+	{
+		name: 'Walter GordonPippo',
+		work: 'Office Manager',
+		photo: 'walter-gordon-office-manager.jpg',
+	},
+    {
+		name: 'Angela Lopez',
+		work: 'Social Media Manager',
+		photo: 'angela-lopez-social-media-manager.jpg',
+	},
+	{
+		name: 'Scott Estrada',
+		work: 'Developer',
+		photo: 'scott-estrada-developer.jpg',
+	},
+	{
+		name: 'Barbara Ramos',
+		work: 'Graphic Designer',
+		photo: 'barbara-ramos-graphic-designer.jpg',
+	},
+];
+
+console.table(arrTeam);
+
+for (let i = 0; i < arrTeam.length; i++) {
+	console.log(`Studente numero ${i}: ${arrTeam[i].name} ${arrTeam[i].work}`);
+    
+    // let img = document.createElement("img");
+    // img.src = `img/${arrTeam[i].photo}`;
+    // cardsTeam.append(img)
+    
+    const eleCards = document.createElement('div');
+    cardsTeam.append(eleCards)
+    eleCards.innerHTML += `<img class="img-fluid + " src="img/${arrTeam[i].photo}" alt="">`
+    eleCards.innerHTML += `<h5 class="text-center + " >${arrTeam[i].name}</h5>`;
+    eleCards.innerHTML += `<p class="text-center + ">${arrTeam[i].work}</p>`;
+
+}
